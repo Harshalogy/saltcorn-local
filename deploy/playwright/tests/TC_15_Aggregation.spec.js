2aa1f1f3a50e98a9937c8b3420d4094c876a4f62
Harshalogy <85222886+Harshalogy@users.noreply.github.com>
2026-03-30 16:43:38 +0530

    Disable 'Show team' visibility check and click
    Comment out visibility check and click actions for 'Show team'

--- deploy/playwright/tests/TC_15_Aggregation.spec.js
+++ deploy/playwright/tests/TC_15_Aggregation.spec.js
@@ -269,9 +269,9 @@ test.describe('E2E Test Suite', () => {
         await page.waitForTimeout(1500);
         // await page.click(pageobject.showTeamspan);
         // await page.getByText('Show team', { exact: true }).click();
-        await expect(page.getByText('Show team', { exact: true })).toBeVisible({ timeout: 10000 });
-        await page.getByText('Show team', { exact: true }).click();
-        await page.click(pageobject.deletebutton);
+        // await expect(page.getByText('Show team', { exact: true })).toBeVisible({ timeout: 10000 });
+        // await page.getByText('Show team', { exact: true }).click();
+        // await page.click(pageobject.deletebutton);
         await functions.drag_And_Drop(pageobject.viewsource, pageobject.target);
         await customAssert('Select show team view in view to show dropdown', async () => {
             await page.click(pageobject.View2Showdropdown);
